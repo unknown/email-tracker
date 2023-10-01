b29f991e4517805e6080b4c04b71cc95cb4a29f6
David Mo <44306450+unknown@users.noreply.github.com>
2023-10-01 14:21:38 -0400

    Add host and port environment variable

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,17 @@ use axum::{
     Router,
 };
 use base64::{engine::general_purpose, Engine as _};
-use std::net::SocketAddr;
+use std::{env, net::SocketAddr};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/image", get(image));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3030));
+    let host = env::var("HOST").unwrap_or("127.0.0.1".to_string());
+    let port = env::var("PORT").unwrap_or("3030".to_string());
+    let addr = format!("{host}:{port}");
     println!("Server started, listening on http://{addr}");
-    axum::Server::bind(&addr)
+    axum::Server::bind(&addr.parse().unwrap())
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
         .await
         .expect("Failed to start server");
